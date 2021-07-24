d6741e62e31040d96f8b6e7be258cf23a7f12ef1
Nur A Alam <nuraalam376@gmail.com>
2021-07-24 08:13:19 +0600

    implemented factory method

---
+++ Factory/Factory.js
@@ -0,0 +1,25 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static get factory() {
+    return new PointFactory();
+  }
+}
+
+class PointFactory {
+  static newCartesianPoint(x, y) {
+    return new Point(x, y);
+  }
+
+  newPolarPoint(rho, theta) {
+    return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+  }
+}
+let p = PointFactory.newCartesianPoint(4, 5);
+console.log(p);
+
+p = Point.factory.newPolarPoint(5, Math.PI / 2);
+console.log(p);
